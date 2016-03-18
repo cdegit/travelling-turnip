a26f377de1ef2ae04a3ff680b58b7e029eebe933
Cassandra de Git <cassandra.m.degit@gmail.com>
2016-03-17 17:38:49 -0700

    Added backbone stuff

---
+++ app.js
@@ -0,0 +1,98 @@
+$(function(){
+	var AppRouter = Backbone.Router.extend({
+		routes: {
+			'meal/:id': 'mealAbout',
+			'meal/:id/recipes': 'mealRecipes',
+			'*actions': 'defaultRoute'
+		}
+	});
+
+	var Meal = Backbone.Model.extend({
+		defaults: function() {
+			return {
+				title: 'Rosemary Foccacia',
+				nearby: true,
+				recipes: true,
+				desc: 'Lorem ipsum',
+				saved: false
+			}
+		},
+
+		toggle: function() {
+			this.save({saved: !this.get("saved")});
+		}
+	});
+
+	var MealList = Backbone.Collection.extend({
+		model: Meal,
+		localStorage: new Backbone.LocalStorage("meals-backbone"),
+		saved: function() {
+			return this.where({saved: true});
+		}
+	});
+
+	var Meals = new MealList;
+
+	var MealView = Backbone.View.extend({
+		tagName: 'div',
+		className: 'c-meal',
+		template: _.template($('#meal-template').html()),
+		detailTemplate: _.template($('#meal-detail-template').html()),
+
+		render: function(detail) {
+			if (detail) {
+				this.$el.html(this.detailTemplate(this.model.toJSON()));
+			} else {
+				this.$el.html(this.template(this.model.toJSON()));
+			}
+      		
+      		return this;
+      	}
+	});
+
+	var AppView = Backbone.View.extend({
+	    el: $("#todoapp"),
+	    initialize: function() {
+	    	this.listenTo(Meals, 'add', this.addOne);
+	    },
+
+	    addAll: function() {
+	    	Meals.each(this.addOne, this);
+	    },
+
+	    addOne: function(meal) {
+	    	var view = new MealView({model: meal});
+     		this.$("#main").append(view.render().el);
+	    }
+	});
+	
+	var App = new AppView;
+
+	var router = new AppRouter;
+
+	router.on('route:mealAbout', function(id) {
+		// render the detail view for that meal
+		if (Meals.get(id)) {
+			var view = new MealView({model: Meals.get(id)});
+ 			$("#main").html(view.render(true).el);
+ 		}
+
+	});
+
+	router.on('route:mealRecipes', function(id) {
+		// a meal has recipes that will be displayed here
+	});
+
+	router.on('route:defaultRoute', function(id) {
+		// Render the meals page
+		Meals.fetch();
+
+		if (!Meals.length) {
+	    	defaultData.meals.forEach(function(meal) {
+				Meals.create(meal);
+	    	});
+	    }
+	});
+
+	Backbone.history.start();
+});
---
+++ default.js
@@ -0,0 +1,61 @@
+var defaultData = {
+	meals: [{
+		id: 1,
+		title: 'Rosemary Focaccia',
+		restrictions: {
+			veggie: true,
+			vegan: true,
+			gf: false
+		},
+		nearby: true,
+		recipes: true,
+		desc: 'Perfect for feeding a crowd, focaccia is a flavorful and easy-to-make Italian flat bread baked in a sheet pan.',
+		saved: false,
+		about: 'Focaccia, known and loved in Italy and abroad, is yeasted flat bread which belongs essentially to the northern shores of the Mediterranean and has its origin in classical antiquity. Early versions were cooked on the hearth of a hot fire, or on a heated tile or earthenware disk, like the related flatbreads. Bakers often puncture the bread with a knife to relieve bubbling on the surface of the bread.',
+		history: 'Many regions of Italy have an inventive range of flavorings they add to their focaccia. For many centuries it has had an association with Christmas Eve and Epiphany. In the Italian context one thing is',
+		recipes: [{
+			title: 'Nonna\'s Focaccia',
+			prepTime: '10 mins',
+			cookTime: '10 mins',
+			saved: false,
+			restrictions: {
+				veggie: true,
+				vegan: true,
+				gf: false
+			},
+			ingredients: [{
+				eng: '2 tablespoons red-wine vinegar',
+				ital: '2 cucchiai di aceto di vino rosso'
+			}]
+		}]
+	},{
+		id: 2,
+		title: 'Radicchio Salad',
+		restrictions: {
+			veggie: true,
+			vegan: true,
+			gf: false
+		},
+		nearby: true,
+		recipes: true,
+		desc: 'Perfect for feeding a crowd, focaccia is a flavorful and easy-to-make Italian flat bread baked in a sheet pan.',
+		saved: false,
+		about: 'Focaccia, known and loved in Italy and abroad, is yeasted flat bread which belongs essentially to the northern shores of the Mediterranean and has its origin in classical antiquity. Early versions were cooked on the hearth of a hot fire, or on a heated tile or earthenware disk, like the related flatbreads. Bakers often puncture the bread with a knife to relieve bubbling on the surface of the bread.',
+		history: 'Many regions of Italy have an inventive range of flavorings they add to their focaccia. For many centuries it has had an association with Christmas Eve and Epiphany. In the Italian context one thing is',
+		recipes: [{
+			title: 'Nonna\'s Focaccia',
+			prepTime: '10 mins',
+			cookTime: '10 mins',
+			saved: false,
+			restrictions: {
+				veggie: true,
+				vegan: true,
+				gf: false
+			},
+			ingredients: [{
+				eng: '2 tablespoons red-wine vinegar',
+				ital: '2 cucchiai di aceto di vino rosso'
+			}]
+		}]
+	}]
+};
